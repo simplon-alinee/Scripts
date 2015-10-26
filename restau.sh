48aa4b10513bf15fa98e7cee43477ccdff725c73
simplon-alinee <a.elwert@hotmail.fr>
2015-10-26 16:26:12 +0100

    save et restau(pastestée)

---
+++ restau.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+    #sudo apt-get update
+    #sudo apt-get install dselect
+    #sudo dselect update
+   #sudo dpkg --set-selections
+    #sudo apt-get -u dselect-upgrade
+tar -xvf /home/ecodair/Bureau/sauvegarde.tar.gz
+
